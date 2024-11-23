76a2d460472793df0060ecf578494d5900783a3c
Tania Kurniasih Febrianti <tania.febrianti.fict@krw.horizon.ac.id>
2024-11-23 14:42:52 +0700

    ini push awal

---
+++ js/doughnutChart.js
@@ -0,0 +1,21 @@
+const ctx2 = document.getElementById("doughnutChart");
+new Chart(ctx2, {
+  type: "doughnut",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
